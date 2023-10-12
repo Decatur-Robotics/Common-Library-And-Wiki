3ab5f86c7fd9d2d2f68b92d052dcf1b37cd6f3b9
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-10-12 16:55:39 -0400

    Formatted everything but modules

--- Robot Code/src/main/java/frc/robot/Robot.java
+++ Robot Code/src/main/java/frc/robot/Robot.java
@@ -9,8 +9,8 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot
 {
-	private Command m_autonomousCommand;
-	private RobotContainer m_robotContainer;
+	private Command autonomousCommand;
+	private RobotContainer robotContainer;
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
@@ -23,7 +23,7 @@ public class Robot extends TimedRobot
 		// and put our
 		// autonomous chooser on the dashboard.
 
-		m_robotContainer = new RobotContainer();
+		robotContainer = new RobotContainer();
 	}
 
 	/**
@@ -61,11 +61,11 @@ public class Robot extends TimedRobot
 	@Override
 	public void autonomousInit()
 	{
-		m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+		autonomousCommand = robotContainer.getAutonomousCommand();
 		// schedule the autonomous command (example)
-		if (m_autonomousCommand != null)
+		if (autonomousCommand != null)
 		{
-			m_autonomousCommand.schedule();
+			autonomousCommand.schedule();
 		}
 	}
 
@@ -81,9 +81,9 @@ public class Robot extends TimedRobot
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
-		if (m_autonomousCommand != null)
+		if (autonomousCommand != null)
 		{
-			m_autonomousCommand.cancel();
+			autonomousCommand.cancel();
 		}
 	}
 
