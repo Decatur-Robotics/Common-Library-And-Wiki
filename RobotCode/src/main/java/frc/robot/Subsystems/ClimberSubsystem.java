b97336e67dea3163bff6228b0c5455dff6ca8098
seankid <97747093+seankid@users.noreply.github.com>
2024-01-13 13:00:31 -0500

    fixed colin

--- RobotCode/src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
+++ RobotCode/src/main/java/frc/robot/Subsystems/ClimberSubsystem.java
@@ -3,19 +3,18 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.lib.core.motors.ITeamTalon;
 import frc.lib.core.motors.TeamTalonFX;
 import frc.robot.constants.ClimberConstants;
 import frc.robot.constants.Ports;
 
 public class ClimberSubsystem extends SubsystemBase {
 
-    public ITeamTalon[] extendMotors;
+    public TeamTalonFX[] extendMotors;
 
-    private ITeamTalon extendMotorLeft;
-    private ITeamTalon extendMotorRight;
+    private TeamTalonFX extendMotorLeft;
+    private TeamTalonFX extendMotorRight;
     
-    public ClimberSubsystem(ITeamTalon motorExtend) {
+    public ClimberSubsystem(TeamTalonFX motorExtend) {
         // sets extension of left and right motors to given extension length
         extendMotorLeft = new TeamTalonFX("Subsystems.Climber.ExtendRight", Ports.CLIMBER_EXTEND_RIGHT_MOTOR);
        extendMotorRight = new TeamTalonFX("Subsystems.Climber.ExtendLeft", Ports.CLIMBER_EXTEND_LEFT_MOTOR);
