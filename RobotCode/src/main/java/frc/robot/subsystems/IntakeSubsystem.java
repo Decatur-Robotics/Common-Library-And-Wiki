9cf18470cf54918ad14ba1059f6560ffbfbe0873
miruaraya <144387687+miruaraya@users.noreply.github.com>
2024-01-23 16:23:49 -0500

    Update IntakeSubsystem.java

--- RobotCode/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
 import frc.robot.constants.Ports;
+
+import com.revrobotics.CANSparkBase.SoftLimitDirection;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.core.motors.TeamSparkMAX;
 
@@ -9,30 +12,37 @@ public class IntakeSubsystem extends SubsystemBase
     private final TeamSparkMAX intakeMotorRight, intakeMotorLeft, intakeMotorCenter;
     private final double MOTOR_SPEED = 0.5;
     private boolean isLowered = false;
+    private float rotationLimitDown;
+    private float rotationLimitUp;
 
-    public IntakeSubsystem(int forwardChannel, int reverseChannel)
+    public IntakeSubsystem(int forwardChannel, int reverseChannel, float rotationLimitDown, float rotationLimitUp)
     {
         intakeMotorRight = new TeamSparkMAX("Intake Motor", Ports.INTAKE_MOTOR_RIGHT);
         intakeMotorLeft = new TeamSparkMAX("Intake Motor", Ports.INTAKE_MOTOR_LEFT);
         intakeMotorCenter = new TeamSparkMAX("Intake Motor", Ports.INTAKE_MOTOR_CENTER);
+        this.rotationLimitDown = rotationLimitDown;
+        this.rotationLimitUp = rotationLimitUp;
 
         intakeMotorLeft.follow(intakeMotorRight);
         intakeMotorLeft.setInverted(true);
+        intakeMotorRight.setInverted(false);
 
-        // intakeMotorLeft.enableSoftLimit (kForward);
-        // intakeMotorRight.enableSoftLimit (kReverse);
+        intakeMotorLeft.setSoftLimit(SoftLimitDirection.kForward, rotationLimitDown);
+        intakeMotorRight.setSoftLimit(SoftLimitDirection.kForward, rotationLimitDown);
 
+        intakeMotorLeft.setSoftLimit(SoftLimitDirection.kReverse, rotationLimitUp);
+        intakeMotorRight.setSoftLimit(SoftLimitDirection.kReverse, rotationLimitUp);
     }
 
-    public void raiseIntake()
-    {
-        // if (isLowered)
-
-    }
-
-    public void lowerIntake()
-    {
-
+    public void raiseOrLowerIntake(){
+        if (isLowered){
+            intakeMotorRight.set(MOTOR_SPEED*-1);
+            isLowered = false;
+        }
+        else{
+            intakeMotorRight.set(MOTOR_SPEED);
+            isLowered = true;
+        }
     }
 
     public void toggleIntakeOn()
@@ -43,6 +53,8 @@ public class IntakeSubsystem extends SubsystemBase
     public void stopIntake()
     {
         intakeMotorCenter.set(0);
+        intakeMotorLeft.set(0);
+        intakeMotorRight.set(0);
     }
 
 }
