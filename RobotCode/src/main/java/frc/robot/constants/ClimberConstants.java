cf4915f75c131a8dddbbfc3a1f879748ca042440
Owen Beck <oebeck@gmail.com>
2024-01-25 17:41:31 -0500

    Changes needed.

--- RobotCode/src/main/java/frc/robot/constants/ClimberConstants.java
+++ RobotCode/src/main/java/frc/robot/constants/ClimberConstants.java
@@ -1,6 +1,7 @@
 package frc.robot.constants;
 
-public final class ClimberConstants {
+public final class ClimberConstants
+{
     public static final double MAX_SPEED = 1, MIN_SPEED = 0.3, SPEED = 0.45;
     public static final double MIN_EXTENSION_RIGHT = -5;
     public static final double MIN_EXTENSION_LEFT = 5;
