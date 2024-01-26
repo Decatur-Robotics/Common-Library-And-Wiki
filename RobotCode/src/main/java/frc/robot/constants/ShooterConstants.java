b00dc3c373a34fb08e38a5119aeb9bf53f5f2868
seankid <97747093+seankid@users.noreply.github.com>
2024-01-26 17:07:55 -0500

    Deleted a file i added manually

--- RobotCode/src/main/java/frc/robot/constants/ShooterConstants.java
+++
@@ -1,9 +0,0 @@
-public class ShooterConstants
-{
-	public static final double SHOOTER_KP = 0.1;
-	public static final double SHOOTER_KI = 0.0;
-	public static final double SHOOTER_KD = 0.1;
-	public static final double FEEDER_KP = 0.1;
-	public static final double FEEDER_KI = 0.0;
-	public static final double FEEDER_KD = 0.1;
-}
