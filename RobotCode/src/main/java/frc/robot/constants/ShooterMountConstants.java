05872394c68876816e73bb68bf85c99e2529e195
Owen Beck <73498409+Beckoe@users.noreply.github.com>
2024-02-03 17:38:21 -0500

    Fixed shooter mount commands and prepared subsystem for auto aiming.

---
+++ RobotCode/src/main/java/frc/robot/constants/ShooterMountConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.constants;
+
+public class ShooterMountConstants {
+
+	public static final double DEGREES_IN_ONE_TICK = 360 / 4096; // Update to reflect gear ratios
+	
+	public static final double SHOOTER_MOUNT_KP = 0.1;
+	public static final double SHOOTER_MOUNT_KI = 0;
+	public static final double SHOOTER_MOUNT_KD = 0.1;
+	public static final double SHOOTER_MOUNT_KF = 0.1;
+	public static final double SHOOTER_MOUNT_CRUISE_VELOCITY = 20000;
+	public static final double SHOOTER_MOUNT_ACCELERATION = 10000;
+
+
+
+}
--- RobotCode/src/main/java/frc/robot/subsystems/ShooterMountSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/ShooterMountSubsystem.java
@@ -2,36 +2,38 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.core.motors.TeamSparkMAX;
+import frc.lib.core.motors.TeamTalonFX;
 import frc.robot.constants.Ports;
+import frc.robot.constants.ShooterMountConstants;
 
 public class ShooterMountSubsystem extends SubsystemBase
 {
 
-	private TeamSparkMAX mainMotor, followMotor;
+	private TeamTalonFX mainMotor, followMotor;
 
 	/** In degrees */
-	private double goalRotation, distance;
-
-	private static final double DEGREES_IN_ONE_TICK = 360 / 42, SPEED = 1;
-	public static final double DEADBAND = 0.5;
+	private double targetRotation; // In degrees
 
 	private boolean autoAim;
 
 	public ShooterMountSubsystem()
 	{
-		mainMotor = new TeamSparkMAX("SHOOTER_MOUNT_MOTOR_LEFT", Ports.SHOOTER_MOUNT_MOTOR_LEFT);
-		followMotor = new TeamSparkMAX("SHOOTER_MOUNT_MOTOR_RIGHT",
+		mainMotor = new TeamTalonFX("SHOOTER_MOUNT_MOTOR_LEFT", Ports.SHOOTER_MOUNT_MOTOR_LEFT);
+		followMotor = new TeamTalonFX("SHOOTER_MOUNT_MOTOR_RIGHT",
 				Ports.SHOOTER_MOUNT_MOTOR_RIGHT);
 
 		followMotor.follow(mainMotor);
 		followMotor.setInverted(true);
 
-		// This is the # of ticks in a rotation and the relative position
-		mainMotor.getEncoder().setPositionConversionFactor(42);
-		mainMotor.getEncoder().setPosition(0);
-		mainMotor.set(0);
+		mainMotor.config_kP(0, ShooterMountConstants.SHOOTER_MOUNT_KP);
+		mainMotor.config_kI(0, ShooterMountConstants.SHOOTER_MOUNT_KI);
+		mainMotor.config_kD(0, ShooterMountConstants.SHOOTER_MOUNT_KD);
+		mainMotor.config_kF(0, ShooterMountConstants.SHOOTER_MOUNT_KF);
+		mainMotor.configMotionCruiseVelocity(ShooterMountConstants.SHOOTER_MOUNT_CRUISE_VELOCITY);
+		mainMotor.configMotionAcceleration(ShooterMountConstants.SHOOTER_MOUNT_ACCELERATION);
+		mainMotor.selectProfileSlot(0, 0);
 
-		goalRotation = 0.0;
+		targetRotation = 0;
 
 		autoAim = false;
 	}
@@ -39,44 +41,20 @@ public class ShooterMountSubsystem extends SubsystemBase
 	@Override
 	public void periodic()
 	{
-		double difference = goalRotation - getCurrentRotation();
-
-		if (Math.abs(difference) < DEADBAND)
+		if (autoAim) 
 		{
-			setMotors(0, "Shooter Mount (Deadbanded): Difference: " + difference + ", Distance: "
-					+ distance);
-			return;
+			
 		}
-
-		double power = distance * Math.sin(difference * Math.PI / distance);
-		power = Math.max(-1, Math.min(power, 1));
-		setMotors(power, "Shooter Mount: Difference: " + difference + ", Distance: " + distance);
 	}
 
-	public void setGoalRotation(double degrees)
+	public void setTargetRotation(double targetRotation)
 	{
-		distance = degrees - getCurrentRotation();
-		goalRotation = degrees;
-	}
-
-	public double getCurrentRotation()
-	{
-		return ticksToDegrees(mainMotor.getEncoder().getPosition());
+		this.targetRotation = targetRotation;
 	}
 
 	private static double degreesToTicks(double degrees)
 	{
-		return degrees / DEGREES_IN_ONE_TICK;
-	}
-
-	public static double ticksToDegrees(double ticks)
-	{
-		return ticks * DEGREES_IN_ONE_TICK;
-	}
-
-	public void setMotors(double power, String reason)
-	{
-		mainMotor.set(Math.max(-1, Math.min(power, 1)) * SPEED, reason);
+		return degrees / ShooterMountConstants.DEGREES_IN_ONE_TICK;
 	}
 
 	public void setAutoAim(boolean autoAim) {
