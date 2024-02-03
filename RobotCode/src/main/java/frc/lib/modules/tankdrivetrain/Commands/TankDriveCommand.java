d6ba747e323b53ebbe12c99aaf99dfc5876aba6f
seankid <97747093+seankid@users.noreply.github.com>
2024-02-03 10:58:44 -0500

    fixed tankdrive's typo

--- RobotCode/src/main/java/frc/lib/modules/tankdrivetrain/Commands/TankDriveCommand.java
+++ RobotCode/src/main/java/frc/lib/modules/tankdrivetrain/Commands/TankDriveCommand.java
@@ -2,44 +2,53 @@ package frc.lib.modules.tankdrivetrain.Commands;
 
 import java.util.function.DoubleSupplier;
 
-im vport edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.modules.tankdrivetrain.TankDrivetrainConstants;
 import frc.lib.modules.tankdrivetrain.TankDrivetrainSubsystem;
-//never
-public class TankDriveCommand extends Command {
 
-    private TankDrivetrainSubsystem drivetrain;
-    private DoubleSupplier leftInputY, rightInputY;
-    
-    public TankDriveCommand(DoubleSupplier leftInputY, DoubleSupplier rightInputY, TankDrivetrainSubsystem drivetrain) {
-        this.leftInputY = leftInputY;
-        this.rightInputY = rightInputY;
-        this.drivetrain = drivetrain;
+// never
+public class TankDriveCommand extends Command
+{
 
-        addRequirements(drivetrain);
-    }
+	private TankDrivetrainSubsystem drivetrain;
+	private DoubleSupplier leftInputY, rightInputY;
 
-    private double calculateDeadZonedPower(double input) {
-        // Dead zone power by setting it to 0 if it is below a threshold
-        if (Math.abs(input) <= TankDrivetrainConstants.DEADZONE_AMOUNT) 
-        {
-            return 0;
-        }
-        else return input;
-    }
+	public TankDriveCommand(DoubleSupplier leftInputY, DoubleSupplier rightInputY,
+			TankDrivetrainSubsystem drivetrain)
+	{
+		this.leftInputY = leftInputY;
+		this.rightInputY = rightInputY;
+		this.drivetrain = drivetrain;
 
-    @Override
-    public void execute() {
-        // Get the dead zoned powers
-        double finalLeftInputY = calculateDeadZonedPower(leftInputY.getAsDouble());
-        double finalRightInputY = calculateDeadZonedPower(rightInputY.getAsDouble());
+		addRequirements(drivetrain);
+	}
 
-        // Get the cubed powers
-        finalLeftInputY = Math.pow(finalLeftInputY, TankDrivetrainConstants.DRIVETRAIN_POWER_EXPONENT);
-        finalRightInputY = Math.pow(finalRightInputY, TankDrivetrainConstants.DRIVETRAIN_POWER_EXPONENT);
+	private double calculateDeadZonedPower(double input)
+	{
+		// Dead zone power by setting it to 0 if it is below a threshold
+		if (Math.abs(input) <= TankDrivetrainConstants.DEADZONE_AMOUNT)
+		{
+			return 0;
+		}
+		else
+			return input;
+	}
 
-        // Set the motor powers in the drivetrain subsystem
-        drivetrain.setMotorPowers(finalLeftInputY, finalRightInputY, "TankDriveCommand Joysticks");
-    }
+	@Override
+	public void execute()
+	{
+		// Get the dead zoned powers
+		double finalLeftInputY = calculateDeadZonedPower(leftInputY.getAsDouble());
+		double finalRightInputY = calculateDeadZonedPower(rightInputY.getAsDouble());
+
+		// Get the cubed powers
+		finalLeftInputY = Math.pow(finalLeftInputY,
+				TankDrivetrainConstants.DRIVETRAIN_POWER_EXPONENT);
+		finalRightInputY = Math.pow(finalRightInputY,
+				TankDrivetrainConstants.DRIVETRAIN_POWER_EXPONENT);
+
+		// Set the motor powers in the drivetrain subsystem
+		drivetrain.setMotorPowers(finalLeftInputY, finalRightInputY, "TankDriveCommand Joysticks");
+	}
 
 }
