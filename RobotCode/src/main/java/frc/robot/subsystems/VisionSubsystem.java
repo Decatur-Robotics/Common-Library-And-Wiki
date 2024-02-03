c16d44a065fcee4df950e6888f932252aefaa930
Owen Beck <73498409+Beckoe@users.noreply.github.com>
2024-02-03 17:58:17 -0500

    Pose estimation.

--- RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -2,49 +2,76 @@ package frc.robot.subsystems;
 
 import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.VisionConstants;
 
 public class VisionSubsystem extends SubsystemBase
 {
 
-    private final PhotonCamera Camera;
+	private final PhotonCamera Camera;
+	private PhotonPoseEstimator robotPoseEstimator, shooterMountPoseEstimator;
 
-    private PhotonPipelineResult latestPipelineResult;
-    private Optional<PhotonTrackedTarget> bestTarget;
+	private PhotonPipelineResult latestPipelineResult;
+	private Optional<PhotonTrackedTarget> bestTarget;
 
-    public VisionSubsystem()
+	public VisionSubsystem()
     {
         Camera = new PhotonCamera(VisionConstants.CameraTableName);
+
+		AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+		Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
+		Transform3d shooterMountToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
+		
+		robotPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, robotToCam);
+		shooterMountPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, shooterMountToCam);
+
     }
 
-    @Override
-    public void periodic()
-    {
-        latestPipelineResult = Camera.getLatestResult();
+	@Override
+	public void periodic()
+	{
+		latestPipelineResult = Camera.getLatestResult();
 
-        // Pay attention to PhotonTargetSortMode!
-        bestTarget = Optional.ofNullable(latestPipelineResult.getBestTarget());
-    }
+		// Pay attention to PhotonTargetSortMode!
+		bestTarget = Optional.ofNullable(latestPipelineResult.getBestTarget());
+	}
 
-    /**
-     * @return the yaw offset of the {@link #bestTarget}, or 0 if no target is found
-     */
-    public double getYawOffset()
-    {
-        return bestTarget.map(PhotonTrackedTarget::getYaw).orElse(0.0);
-    }
+	/**
+	 * @return the yaw offset of the {@link #bestTarget}, or 0 if no target is found
+	 */
+	public double getYawOffset()
+	{
+		return bestTarget.map(PhotonTrackedTarget::getYaw).orElse(0.0);
+	}
 
-    /**
-     * @return the pitch offset of the {@link #bestTarget} , or 0 if no target is found
-     */
-    public double getPitchOffset()
-    {
-        return bestTarget.map(PhotonTrackedTarget::getPitch).orElse(0.0);
-    }
+	/**
+	 * @return the pitch offset of the {@link #bestTarget} , or 0 if no target is found
+	 */
+	public double getPitchOffset()
+	{
+		return bestTarget.map(PhotonTrackedTarget::getPitch).orElse(0.0);
+	}
+
+	public Optional<EstimatedRobotPose> getRobotPose()
+	{
+		return robotPoseEstimator.update();
+	}
+
+	public Optional<EstimatedRobotPose> getShooterMountPose()
+	{
+		return shooterMountPoseEstimator.update();
+	}
 
 }
