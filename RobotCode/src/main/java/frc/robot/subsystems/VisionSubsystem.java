ca03d95d6bef0f8402e28da46e26cc5521c228d9
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-04 15:26:00 -0500

    Added angle to pose method

--- RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -23,89 +23,107 @@ import frc.robot.constants.VisionConstants;
 public class VisionSubsystem extends SubsystemBase
 {
 
-	private final PhotonCamera Camera;
-	private PhotonPoseEstimator robotPoseEstimator, shooterMountPoseEstimator;
+    private final PhotonCamera Camera;
+    private PhotonPoseEstimator robotPoseEstimator, shooterMountPoseEstimator;
 
-	private PhotonPipelineResult latestPipelineResult;
-	private Optional<PhotonTrackedTarget> bestTarget;
+    private PhotonPipelineResult latestPipelineResult;
+    private Optional<PhotonTrackedTarget> bestTarget;
 
-	public VisionSubsystem()
-	{
-		Camera = new PhotonCamera(VisionConstants.CameraTableName);
+    public VisionSubsystem()
+    {
+        Camera = new PhotonCamera(VisionConstants.CameraTableName);
 
-		AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo
-				.loadAprilTagLayoutField();
-		Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5),
-				new Rotation3d(0, 0, 0)); // Tune to robot
-		Transform3d shooterMountToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5),
-				new Rotation3d(0, 0, 0)); // Tune to robot
+        AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo
+                .loadAprilTagLayoutField();
+        Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5),
+                new Rotation3d(0, 0, 0));
+        Transform3d shooterMountToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5),
+                new Rotation3d(0, 0, 0));
 
-		robotPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
-				PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, robotToCam);
-		shooterMountPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
-				PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, shooterMountToCam);
+        robotPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
+                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, robotToCam);
+        shooterMountPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
+                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, shooterMountToCam);
+    }
 
-	}
+    @Override
+    public void periodic()
+    {
+        latestPipelineResult = Camera.getLatestResult();
 
-	@Override
-	public void periodic()
-	{
-		latestPipelineResult = Camera.getLatestResult();
+        // Pay attention to PhotonTargetSortMode!
+        bestTarget = Optional.ofNullable(latestPipelineResult.getBestTarget());
+    }
 
-		// Pay attention to PhotonTargetSortMode!
-		bestTarget = Optional.ofNullable(latestPipelineResult.getBestTarget());
-	}
+    /**
+     * @return the yaw offset of the {@link #bestTarget}, or 0 if no target is found
+     */
+    public double getYawOffset()
+    {
+        return bestTarget.map(PhotonTrackedTarget::getYaw).orElse(0.0);
+    }
 
-	/**
-	 * @return the yaw offset of the {@link #bestTarget}, or 0 if no target is found
-	 */
-	public double getYawOffset()
-	{
-		return bestTarget.map(PhotonTrackedTarget::getYaw).orElse(0.0);
-	}
+    /**
+     * @return the pitch offset of the {@link #bestTarget} , or 0 if no target is found
+     */
+    public double getPitchOffset()
+    {
+        return bestTarget.map(PhotonTrackedTarget::getPitch).orElse(0.0);
+    }
 
-	/**
-	 * @return the pitch offset of the {@link #bestTarget} , or 0 if no target is found
-	 */
-	public double getPitchOffset()
-	{
-		return bestTarget.map(PhotonTrackedTarget::getPitch).orElse(0.0);
-	}
+    public Optional<Pose2d> getRobotPose()
+    {
+        Optional<EstimatedRobotPose> estPose = robotPoseEstimator.update();
+        if (estPose.isPresent())
+        {
+            EstimatedRobotPose pose = estPose.get();
+            Pose3d pose3d = pose.estimatedPose;
 
-	public Optional<Pose2d> getRobotPose()
-	{
-		Optional<EstimatedRobotPose> estPose = robotPoseEstimator.update();
-		if (estPose.isPresent())
-		{
-			EstimatedRobotPose pose = estPose.get();
-			Pose3d pose3d = pose.estimatedPose;
+            // We only care about the x and z, and the yaw
+            Rotation2d rot2d = new Rotation2d(pose3d.getRotation().getY());
+            Pose2d pose2d = new Pose2d(pose3d.getX(), pose3d.getZ(), rot2d);
 
-			// We only care about the x and z, and the yaw
-			Rotation2d rot2d = new Rotation2d(pose3d.getRotation().getY());
-			Pose2d pose2d = new Pose2d(pose3d.getX(), pose3d.getZ(), rot2d);
+            return Optional.of(pose2d);
+        }
 
-			return Optional.of(pose2d);
-		}
+        return Optional.empty();
+    }
 
-		return Optional.empty();
-	}
+    public Optional<Pose2d> getShooterMountPose()
+    {
+        Optional<EstimatedRobotPose> estPose = shooterMountPoseEstimator.update();
+        if (estPose.isPresent())
+        {
+            EstimatedRobotPose pose = estPose.get();
+            Pose3d pose3d = pose.estimatedPose;
 
-	public Optional<Pose2d> getShooterMountPose()
-	{
-		Optional<EstimatedRobotPose> estPose = shooterMountPoseEstimator.update();
-		if (estPose.isPresent())
-		{
-			EstimatedRobotPose pose = estPose.get();
-			Pose3d pose3d = pose.estimatedPose;
+            // We only care about the x and z, and the yaw
+            Rotation2d rot2d = new Rotation2d(pose3d.getRotation().getY());
+            Pose2d pose2d = new Pose2d(pose3d.getX(), pose3d.getZ(), rot2d);
 
-			// We only care about the x and z, and the yaw
-			Rotation2d rot2d = new Rotation2d(pose3d.getRotation().getY());
-			Pose2d pose2d = new Pose2d(pose3d.getX(), pose3d.getZ(), rot2d);
+            return Optional.of(pose2d);
+        }
 
-			return Optional.of(pose2d);
-		}
+        return Optional.empty();
+    }
 
-		return Optional.empty();
-	}
+    public double pointTowardsPose(Pose3d targetPose)
+    {
+        Optional<Pose2d> robotPose = getRobotPose();
+
+        if (robotPose.isEmpty())
+        {
+            return 0.0;
+        }
+
+        Pose2d robotPose2d = robotPose.get();
+
+        // Calculate the angle to the target
+        Pose2d distance = new Pose2d(targetPose.getX() - robotPose2d.getX(),
+                targetPose.getY() - robotPose2d.getY(), new Rotation2d());
+
+        // Use the inverse tangent to calculate the angle
+        return Math.atan2(distance.getY(), distance.getX());
+    }
 
 }
