5ef2c6965492810736e2271494c6e339d1dde1ce
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-15 22:20:56 -0500

    Renamed hypotenuse

--- RobotCode/src/main/java/frc/robot/commands/AimShooterCommand.java
+++ RobotCode/src/main/java/frc/robot/commands/AimShooterCommand.java
@@ -89,28 +89,32 @@ public class AimShooterCommand extends Command
 		double groundDistance = speakerPose.getDistance(shooterMountPose.getTranslation());
 
 		// Calculate the distance from the shooter mount to speaker opening
-		double hypotenuse = Math.sqrt(Math.pow(ShooterMountConstants.SHOOTER_MOUNT_TO_SPEAKER, 2)
+		// Essentially how far the note will travel
+		double shotDistance = Math.sqrt(Math.pow(ShooterMountConstants.SHOOTER_MOUNT_TO_SPEAKER, 2)
 				+ Math.pow(groundDistance, 2));
 
 		// Calculate shooter mount pose adjusted by velocity and distance to speaker
 		Translation2d velocityAdjustedShooterMountPose = new Translation2d(
-				shooterMountPose.getX()
-						+ ((hypotenuse * ShooterMountConstants.EJECTED_NOTE_VELOCITY)
-								* chassisVelocity.getX()),
+				shooterMountPose.getX() + (shotDistance
+						* ShooterMountConstants.EJECTED_NOTE_VELOCITY * chassisVelocity.getX()),
 				shooterMountPose.getY()
-						+ ((hypotenuse / ShooterMountConstants.EJECTED_NOTE_VELOCITY)
-								* chassisVelocity.getY()));
+						+ (shotDistance / ShooterMountConstants.EJECTED_NOTE_VELOCITY)
+								* chassisVelocity.getY());
 
 		// Recalculate ground distance and hypotenuse adjusted for velocity
-		double velocityAdjustedGroundDistance = speakerPose.getDistance(velocityAdjustedShooterMountPose);
-		double velocityAdjustedHypotenuse = Math.sqrt(Math.pow(ShooterMountConstants.SHOOTER_MOUNT_TO_SPEAKER, 2)
-				+ Math.pow(velocityAdjustedGroundDistance, 2));
+		double velocityAdjustedGroundDistance = speakerPose
+				.getDistance(velocityAdjustedShooterMountPose);
+		double velocityAdjustedHypotenuse = Math
+				.sqrt(Math.pow(ShooterMountConstants.SHOOTER_MOUNT_TO_SPEAKER, 2)
+						+ Math.pow(velocityAdjustedGroundDistance, 2));
 
 		// Calculate the target rotation of the shooter mount
 		double targetRotation = Math.atan(
-				ShooterMountConstants.SHOOTER_MOUNT_TO_SPEAKER / velocityAdjustedGroundDistance) * (180 / Math.PI);
+				ShooterMountConstants.SHOOTER_MOUNT_TO_SPEAKER / velocityAdjustedGroundDistance)
+				* (180 / Math.PI);
 
-		shooterMount.setTargetRotation(targetRotation + gravityCompensationTreeMap.get(velocityAdjustedHypotenuse));
+		shooterMount.setTargetRotation(
+				targetRotation + gravityCompensationTreeMap.get(velocityAdjustedHypotenuse));
 	}
 
 	@Override
