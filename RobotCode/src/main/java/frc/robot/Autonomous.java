5d7ddf532242ffb112b5ade0ca0e8f600ce62118
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-22 15:59:42 -0500

    Removed unused imports in Autonomous

--- RobotCode/src/main/java/frc/robot/Autonomous.java
+++ RobotCode/src/main/java/frc/robot/Autonomous.java
@@ -9,14 +9,9 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.lib.core.ILogSource;
 import frc.lib.modules.swervedrive.SwerveConstants;
-import frc.lib.modules.swervedrive.SwerveDriveSubsystem;
-import frc.lib.modules.swervedrive.Commands.AutoAimSwerveCommand;
-import frc.lib.modules.swervedrive.Commands.DriveDistanceAuto;
-import frc.robot.commands.AimShooterCommand;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.RotateShooterMountToPositionCommand;
 import frc.robot.commands.ShootCommand;
@@ -25,7 +20,6 @@ import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterMountSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.VisionSubsystem;
 
 /**
  * <p>
