40619a898c9bb7dd060d6418b28f8f6afcce72be
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-29 17:52:55 -0500

    Elaborated on Untested javadoc
    Signed-off-by: Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>

--- RobotCode/src/main/java/frc/lib/core/Untested.java
+++ RobotCode/src/main/java/frc/lib/core/Untested.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Target;
 
 /**
  * Untested marks code that has not been tested on a robot. If a class is marked as untested, its methods do not need to
- * also be marked as untested.
+ * also be marked as untested. It does not need to be applied to constants classes.
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
 public @interface Untested {
