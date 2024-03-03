2bed25282baa07d29269ebe5b3cc2234e69e0432
Owen Beck <73498409+Beckoe@users.noreply.github.com>
2024-03-02 22:13:59 -0500

    Cleaned up vision subsystem.

--- RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,28 +1,12 @@
 package frc.robot.subsystems;
 
-import java.util.Optional;
-
-import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.modules.swervedrive.SwerveDriveSubsystem;
-import frc.robot.RobotContainer;
 import frc.robot.constants.Constants;
-import frc.robot.constants.ShooterMountConstants;
 import frc.robot.constants.VisionConstants;
 
 public class VisionSubsystem extends SubsystemBase
@@ -30,8 +14,6 @@ public class VisionSubsystem extends SubsystemBase
 
 	private final PhotonCamera Camera;
 	private PhotonPoseEstimator robotPoseEstimator;
-
-	private PhotonPipelineResult latestPipelineResult;
 
 	private final SwerveDriveSubsystem Swerve;
 
@@ -48,8 +30,6 @@ public class VisionSubsystem extends SubsystemBase
 	@Override
 	public void periodic()
 	{
-		latestPipelineResult = Camera.getLatestResult();
-
 		Swerve.updatePoseWithVision(robotPoseEstimator.update());
 	}
 
