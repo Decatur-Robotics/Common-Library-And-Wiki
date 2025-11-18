55950b67b7a5210a3becff22594bf3195e98fcca
Charles Kennedy <82charkenn@csdecatur.net>
2025-11-18 16:43:57 -0500

    🗣️🗣️🗣️🗣️🗣️🗣️

---
+++ RobotCode/src/main/java/frc/robot/;;;;;;;;;;;;;/am.java
@@ -0,0 +1,16 @@
+import java.security.PublicKey;
+
+package frc.robot.;;;;;;;;;;;;;;
+
+public class am-14 U4_PW4_Cons
+{
+	PublicKey key;
+	pabeless
+	public static final double MAX_SPEED = 5.0;
+	public static final double MAX_ACCELERATION = 3.0;
+
+	private am-14U4_PW4_Cons() {
+	}
+
+	long mainMotor;
+}
